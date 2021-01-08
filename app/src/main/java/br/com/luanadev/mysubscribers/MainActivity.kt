9ea97ad32f531326034c819ca7325fa0383a288b
Luana Souza <luana.souza@ifood.com.br>
2021-01-08 13:38:36 -0300

    creating project

---
+++ app/src/main/java/br/com/luanadev/mysubscribers/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.luanadev.mysubscribers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.luanadev.mysubcribers.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
