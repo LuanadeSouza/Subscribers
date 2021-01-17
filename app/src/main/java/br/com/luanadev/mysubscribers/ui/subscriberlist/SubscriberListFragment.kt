c4dc072fde42638f4e45f9134f6c033bb3b9ed8c
Luana Souza <luana.souza@ifood.com.br>
2021-01-17 15:40:43 -0300

    Tela de Listagem de Subscribes

---
+++ app/src/main/java/br/com/luanadev/mysubscribers/ui/subscriberlist/SubscriberListFragment.kt
@@ -0,0 +1,9 @@
+package br.com.luanadev.mysubscribers.ui.subscriberlist
+
+import androidx.fragment.app.Fragment
+import br.com.luanadev.mysubcribers.R
+
+class SubscriberListFragment : Fragment(R.layout.subscriber_list_fragment) {
+
+
+}
